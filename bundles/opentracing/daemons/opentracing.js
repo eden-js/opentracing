d3ec9565c16c374ac5252b072df7d33d831a2617
EdenCoder <alex@adlist.co.nz>
2019-05-31 09:32:09 +1000

    initial commit

---
+++ bundles/opentracing/daemons/opentracing.js
@@ -0,0 +1,58 @@
+
+// Require dependencies
+const config      = require('config');
+const Daemon      = require('controller');
+const middleware  = require('express-opentracing');
+const opentracing = require('opentracing');
+
+// Require models
+const Notification = model('notification');
+
+/**
+ * Build notification controller
+ */
+class PrometheusDaemon extends Daemon {
+  /**
+   * Construct notification controller
+   */
+  constructor() {
+    // Run super
+    super();
+    
+  }
+
+
+  // ////////////////////////////////////////////////////////////////////////////
+  //
+  // INITIALIZE METHODS
+  //
+  // ////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * builds notification controller
+   */
+  static initialize(eden) {
+    // create tracer
+    const data = {
+      tracer : new opentracing.Tracer(),
+    };
+
+    // hook tracer
+    await eden.hook('opentracing.tracer', data);
+
+    // on render
+    eden.pre('eden.router.create', (app) => {
+      // use metrics
+      app.use(middleware({
+        tracer : data.tracer,
+      }));
+    });
+  }
+}
+
+/**
+ * Export prometheus daemon
+ *
+ * @type {PrometheusDaemon}
+ */
+module.exports = PrometheusDaemon;
---
+++ edenconfig.js
@@ -0,0 +1,10 @@
+// create config object
+const config = {};
+
+// default company config
+config.opentracing = {
+  
+};
+
+// export config
+module.exports = config;
